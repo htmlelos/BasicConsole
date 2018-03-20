a9d095c2f1bff005d29f65eefdf9ed57cd603bcd
Sergio Lucero <sergiolucero73@gmail.com>
2018-03-20 19:45:59 -0300

    added users page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const fetch = require('node-fetch')
 
 const server = express()
 
@@ -7,12 +8,27 @@ const server = express()
 server.set('view engine', 'pug')
 server.set("views", path.join(__dirname, "views"))
 
-server.use("/static", express.static(path.join(__dirname, "public")))
+server.use('/static', express.static(path.join(__dirname, "public")))
 //Rutas
-server.use('/', function(request, response) {
-    response.render('home', { title: 'Titulo', message: 'Hola Mundo!'})
+server.get('/', function (request, response) {
+    // console.log('INICIO');
+    response.render('home', { title: 'Titulo', message: 'Hola Mundo!' })
 })
 
-server.listen(4000, function() {
+server.get('/users', function (request, response) {
+    console.log('UI_USER');
+
+    fetch('http://localhost:3000/users', { method: 'GET', headers: {} })
+        .then(function(response) {return response.json()})
+        .then(function(result){
+            console.log(result.users);
+            response.render('users', {users: result.users})
+        })
+        .catch(function(error){
+            response.render('error')
+        })
+})
+
+server.listen(4000, function () {
     console.log('Servidor ejecutandose en el puerto 4000');
 })
