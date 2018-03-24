a05e48bbf654d487a4cd7b946c76b78ad912867e
sergiolucero73 <sergiolucero73@gmail.com>
2018-03-24 15:42:28 -0300

    call users endpoint modified

--- server.js
+++ server.js
@@ -12,17 +12,29 @@ server.use('/static', express.static(path.join(__dirname, "public")))
 //Rutas
 server.get('/', function (request, response) {
     // console.log('INICIO');
-    response.render('home', { title: 'Consola' })
+    response.render('home', {
+        title: 'Consola'
+    })
 })
 
 server.get('/users', function (request, response) {
-    console.log('UI_USER');
-
-    fetch('http://localhost:3000/users', { method: 'GET', headers: {} })
-        .then(function (response) { return response.json() })
+    const body = {
+        fields: '-_id name password email'
+    }
+    fetch('http://localhost:3000/users', {
+            method: 'POST',
+            body: JSON.stringify(body),
+            headers: {'Content-Type': 'application/json'}
+        })
+        .then(function (response) {
+            return response.json()
+        })
         .then(function (result) {
             console.log(result.users);
-            response.render('users', { title: 'Usuarios', list: result.users })
+            response.render('users', {
+                title: 'Usuarios',
+                list: result.users
+            })
         })
         .catch(function (error) {
             response.render('error')
